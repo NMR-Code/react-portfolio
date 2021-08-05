e830ad655e87f756ce60dc9895e14ca6076d33ca
Nelson Rosado <nelsonog827@gmail.com>
2021-08-05 12:46:18 -0400

    uplaoding almost finished product

---
+++ src/resumeData.js
@@ -0,0 +1,104 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Rahul Bhatia",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/808melo/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/NMR-Code",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"http://twitter.com/wrongrosado",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a student at The University of Miami and pursuing my career in tech from here. I am a Full Stack Web Developer, currently diving deeper into React. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Miami, FL",
+    "website":"https://NMR-Code.github.io",
+    "education":[
+      {
+        "UniversityName":"University Of Miami",
+        "specialization":"Full Stack Software Dev",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"UM Coding Bootcamp",
+        "specialization":"Full Stack Sofware Dev",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2021",
+        "Achievements":"Gained knowledge in various technologies"
+      }
+    ],
+    "skillsDescription":"I have experience with the MERN stack and related middleware.",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"mySQL"
+      },
+      {
+        "skillname":"graphQL"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Workout Tracker",
+        "description":"A basic app used to record workouts",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
